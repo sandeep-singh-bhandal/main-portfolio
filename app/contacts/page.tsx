b87a9a8220b69b84d924b57412bffa63d41aeb81
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-05-12 16:47:54 +0530

    data updated

---
+++ app/contacts/page.tsx
@@ -0,0 +1,18 @@
+import Contact from '@/components/main/Contact'
+import Footer from '@/components/main/Footer'
+import Navbar from '@/components/main/Navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20 max-md:gap-10">
+        <Navbar />
+        <Contact />
+        <Footer />
+      </div>
+    </main>
+  )
+}
+
+export default page
