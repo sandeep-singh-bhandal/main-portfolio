65a351abc42db5a02cb59ffe5c8a480a55d6eff8
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-05-14 18:30:58 +0530

    bugfix

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -58,14 +58,14 @@ export const BentoGridItem = ({
 
   const [copied, setCopied] = useState(false);
 
-  const defaultOptions = {
-    loop: copied,
-    autoplay: copied,
-    // animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+  // const defaultOptions = {
+  //   loop: copied,
+  //   autoplay: copied,
+  //   // animationData: animationData,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice",
+  //   },
+  // };
 
   const handleCopy = () => {
     const text = "sandeepbhandaldev@gmail.com";
