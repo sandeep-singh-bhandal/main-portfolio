a5e7477bf63628d71c89cf84d7e6d7cc24e2990a
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-05-14 22:39:18 +0530

    bugfix

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -29,65 +29,62 @@ const Projects = () => {
       </motion.h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16">
         {ProjectsData.projects.map((item, index) => (
-          <div
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={slideInFromBottom(index * 0.5)}
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
           >
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={slideInFromBottom(index * 0.5)}
-            >
-              <PinContainer title={item.title} href={item.link}>
-                <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[24vh] lg:h-[30vh] mb-10">
-                  <Image
-                    height={1000}
-                    width={1000}
-                    src={item.img}
-                    alt="cover"
-                    className="z-10 absolute bottom-0 rounded-xl"
-                  />
+            <PinContainer title={item.title} href={item.link}>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[24vh] lg:h-[30vh] mb-10">
+                <Image
+                  height={1000}
+                  width={1000}
+                  src={item.img}
+                  alt="cover"
+                  className="z-10 absolute bottom-0 rounded-xl"
+                />
+              </div>
+
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {item.title}
+              </h1>
+
+              <p
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                style={{
+                  color: "#BEC1DD",
+                  margin: "1vh 0",
+                }}
+              >
+                {item.des}
+              </p>
+
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {item.iconLists.map((icon, index) => (
+                    <div
+                      key={index}
+                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index + 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt="icon5" className="p-2" />
+                    </div>
+                  ))}
                 </div>
 
-                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                  {item.title}
-                </h1>
-
-                <p
-                  className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                  style={{
-                    color: "#BEC1DD",
-                    margin: "1vh 0",
-                  }}
-                >
-                  {item.des}
-                </p>
-
-                <div className="flex items-center justify-between mt-7 mb-3">
-                  <div className="flex items-center">
-                    {item.iconLists.map((icon, index) => (
-                      <div
-                        key={index}
-                        className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                        style={{
-                          transform: `translateX(-${5 * index + 2}px)`,
-                        }}
-                      >
-                        <img src={icon} alt="icon5" className="p-2" />
-                      </div>
-                    ))}
-                  </div>
-
-                  <div className="flex justify-center items-center">
-                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                      Check Live Site
-                    </p>
-                    <FaLocationArrow className="ms-3" color="#CBACF9" />
-                  </div>
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
-              </PinContainer>
-            </motion.div>
-          </div>
+              </div>
+            </PinContainer>
+          </motion.div>
         ))}
       </div>
     </div>
